184084fba61e93dd84671b8050c7cda674950765
Arafie2603 <arrafiesetiawan@gmail.com>
2023-10-21 15:21:29 +0700

    Menambahkan config untuk Models Akun dan Transa

---
+++ app/Models/Akun.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Akun extends Model
+{
+    use HasFactory;
+    protected $table = "akuns";
+    protected $primaryKey = "id_akun";
+    protected $fillable = ['id_akun','no_telp', 'poin', 'saldo'];
+    public $timestamps = true;
+    public $incrementing = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id', 'id');
+    }
+}
