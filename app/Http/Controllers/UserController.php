4bc2feacbc9a1e41f5daa01e841afff4198a399c
Arafie2603 <arrafiesetiawan@gmail.com>
2023-10-22 01:15:03 +0700

    menambahkan dan edit config UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,10 +14,27 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = User::with('akun')->find(Auth::user()->id);
-        $saldo = $user->akun->saldo;
 
-        return view('pages.dashboard', compact('user', 'saldo'));
+        $user = User::with('akun')->find(Auth::user()->id);
+
+        if ($user->role_id == 1) {
+            $data_user = User::paginate(5);
+            $user = User::where('id', '=', Auth::user()->id)->firstOrFail();
+            $lastId = $user->id;
+            return view('admin.user', compact('user', 'data_user', 'lastId'));
+        } else {
+            try {
+                $saldo = $user->akun->saldo;
+            } catch (\Throwable $th) {
+                $saldo = 0;
+            }
+            return view('pages.user', compact('saldo'));
+        }
+    }
+    public function admin()
+    {
+        $user = User::all();
+        return view('admin.user', compact('user'));
     }
 
     /**
@@ -33,6 +50,31 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        request()->validate(
+            [
+                // 'password' => 'required',
+                // 'password_ulang' => 'same:password',
+                'password' => 'required|confirmed'
+            ]
+        );
+
+        $user = User::with('akun')->all();
+        $data_user = User::where('id', '=', $request->id)->first();
+        if ($data_user) {
+            return back()->with('info', 'Duplikat data (Data Pegawai sudah terdaftar di dalam sistem)');
+        }
+
+        $user->id = $request->id;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->email->saldo = $request->saldo;
+        $user->email->poin = $request->poin;
+        $user->password = bcrypt($request->password);
+
+
+
+        $user->save();
+        return back()->with('success', 'Data Berhasil ditambah');
     }
 
     /**
@@ -56,6 +98,21 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        request()->validate([
+            'password_confirmation' => 'same:password_baru',
+        ]);
+
+        $user = User::findorfail($id);
+        $user->id = $request->id;
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        if($request->password_baru) {
+            $user->password = bcrypt($request->password_baru);
+        }
+
+        $user->save();
+        return back()->with('success', 'Data berhasil diubah!');
     }
 
     /**
@@ -63,5 +120,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
+        $user = User::where('id','=',$id)->firstOrFail();
+        $user->delete();
+        return back()->with('info', 'Data berhasil dihapus');
     }
 }
