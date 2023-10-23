4887f4aaafbc212ef78836e07f39948592ee1387
Arafie2603 <arrafiesetiawan@gmail.com>
2023-10-23 12:02:35 +0700

    menambahkan function relasi dan definisi tabel

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -8,11 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     use HasFactory;
-    
+    protected $table = "transaksis";
+    protected $primaryKey = "id_transaksi";
+    protected $fillable = ['id_transaksi','total_item', 'total_harga', 'akun_id'];
+    public $timestamps = true;
+    public $incrementing = false;
 
-    public function user()
+    public function akuns()
     {
     
-        return $this->belongsTo(User::class, 'akun_id', 'id_akun');
+        return $this->belongsTo(Akun::class, 'akun_id', 'id_akun');
+    }
+
+    public function transaksidetail()
+    {
+        return $this->hasMany(TransaksiDetail::class);
     }
 }
